c80a8311e0d14ba3a63818d48b49c66e85d9cc9b
Sean Pinote <sean.pinote@usjr.edu.ph>
2021-09-06 11:35:05 +0800

    initial

---
+++ 2_JS/VarialType.js
@@ -0,0 +1,14 @@
+var number = 1;
+var a = [1, 2, 3, 4, 5, 6];
+var b = { one: 1, "two": "two", three: [3] };
+var c;
+var d = null;
+
+console.log(typeof b.three);
+console.log(`The variable ${b} is of type ${typeof number}`);
+console.lob(b);
+b.four = "Hi all";
+console.log(b);
+b["five"] = ["HAHAHHA", "MEOW"];
+console.log(b);
+console.log(b.six);
