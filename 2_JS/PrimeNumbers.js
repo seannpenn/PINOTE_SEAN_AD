b048328355fdc1d46f72a1923c5c2fc597f6672a
Sean Pinote <sean.pinote@usjr.edu.ph>
2021-09-08 08:36:19 +0800

    slight changes

--- 2_JS/PrimeNumbers.js
+++ 2_JS/PrimeNumbers.js
@@ -1,13 +1,18 @@
-// var number = 5;
+var number = 5;
 
-// for (var x = 0; x < 2; x++) {
-//     if (number % 2 == 0) {
-//         console.log('not prime');
-//     } else {
-//         console.log('prime');
-//     }
-//     number = 1;
-// }
+for (var x = 0; x < 2; x++) {
+    if (number % 2 == 0) {
+        console.log(`${number} is a prime number? false`);
 
-console.log("5 is a prime number?", 5 % 2 == 0 ? "false" : "true");
-console.log("6 is a prime number?", 6 % 2 == 0 ? "false" : "true");
+    } else {
+
+        console.log(`${number} is a prime number? true`);
+    }
+    number = 6;
+}
+
+//or
+
+
+// console.log("5 is a prime number?", 5 % 2 == 0 ? "false" : "true");
+// console.log("6 is a prime number?", 6 % 2 == 0 ? "false" : "true");
