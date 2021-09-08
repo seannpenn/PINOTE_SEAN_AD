42d5483742f652937b2cb9ffc5ea1be2995df810
Sean Pinote <sean.pinote@usjr.edu.ph>
2021-09-08 10:56:14 +0800

    another slight changes

--- 2_JS/MultiplicationTable.js
+++ 2_JS/MultiplicationTable.js
@@ -4,6 +4,12 @@ var max = 10;
 for (var i = 1; i <= `${max}`; i++) {
     for (var x = 1; x <= `${max}`; x++) {
         product += (i * x) + '  ';
+        if ((i * x) < 10) {
+            product += '  ';
+        } else {
+            product += ' ';
+        }
+
     }
     product += '\n';
 }
